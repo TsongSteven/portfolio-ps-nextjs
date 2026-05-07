df7a32d3c879c767d818596a0398d7945dd38b44
Pranan Subba <pranan.steven@ymail.com>
2026-05-07 15:44:14 +0530

    Logout and access denied to non logged in user

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,12 @@
 import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
-export const proxy = auth((req) => {
-  return;
+export default auth((req) => {
+    if (!req.auth) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
 });
+
+export const config = {
+    matcher: ["/admin/:path*"],
+};
