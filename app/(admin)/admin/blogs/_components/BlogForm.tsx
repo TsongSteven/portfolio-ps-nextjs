305b52981ed0e2d583c66819c19ecb54c68881f6
Pranan Subba <pranan.steven@ymail.com>
2026-05-12 18:18:55 +0530

    Made post blog or story

--- app/(admin)/admin/blogs/_components/BlogForm.tsx
+++ app/(admin)/admin/blogs/_components/BlogForm.tsx
@@ -1,5 +1,12 @@
-"use client"
-import { Field, FieldError, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+"use client";
+import {
+  Field,
+  FieldContent,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import QuillEditor from "@/components/quill-editor";
 import { Button } from "@/components/ui/button";
@@ -9,82 +16,117 @@ import saveBlog from "@/lib/save-post";
 import { zodResolver } from "@hookform/resolvers/zod";
 import z from "zod";
 import editBlog from "@/lib/edit-post";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const stripHtml = (html: string) => {
-    return html.replace(/<[^>]*>/g, "").trim();
+  return html.replace(/<[^>]*>/g, "").trim();
 };
 
 const blogSchema = z.object({
-    title: z.string()
-        .min(30, "Title must be at least 30 characters.")
-        .max(60, "Title must be at leaset 60 character."),
-    content: z.string()
-        .refine((val) => stripHtml(val).length > 0, {
-            message: "Content is required",
-        }),
-})
-
+  title: z
+    .string()
+    .min(30, "Title must be at least 30 characters.")
+    .max(60, "Title must be at leaset 60 character."),
+  post_type: z.string().min(1, "Please select your spoken language."),
+  content: z.string().refine((val) => stripHtml(val).length > 0, {
+    message: "Content is required",
+  }),
+});
 
 export default function BlogForm({ blogData }: { blogData?: any }) {
-    const form = useForm<z.infer<typeof blogSchema>>({
-        resolver: zodResolver(blogSchema),
-        defaultValues: {
-            title: blogData?.title || "",
-            content: blogData?.content || ""
-        }
-    })
+  const form = useForm<z.infer<typeof blogSchema>>({
+    resolver: zodResolver(blogSchema),
+    defaultValues: {
+      title: blogData?.title || "",
+      content: blogData?.content || "",
+      post_type: blogData?.postType || "",
+    },
+  });
 
-    function onSubmit(data: z.infer<typeof blogSchema>) {
-        if (blogData) {
-            editBlog(data.title, data.content, blogData.id)
-        } else {
-            saveBlog(data.title, data.content)
-        }
+  function onSubmit(data: z.infer<typeof blogSchema>) {
+    if (blogData) {
+      editBlog(data.title, data.post_type, data.content, blogData.id);
+    } else {
+      saveBlog(data.title, data.post_type, data.content);
     }
+  }
 
-    return (
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FieldSet>
-                <FieldGroup>
-                    <FieldGroup>
-                        <Controller
-                            name="title"
-                            control={form.control}
-                            render={({ field, fieldState }) => (
-                                <Field data-invalid={fieldState.invalid}>
-                                    <FieldLabel>Title</FieldLabel>
-                                    <Input {...field} type="text" />
-                                    {fieldState.error && (
-                                        <FieldError errors={[fieldState.error]} />
-                                    )}
-                                </Field>
-                            )}
-                        />
-                        <Controller
-                            name="content"
-                            control={form.control}
-                            render={({ field, fieldState }) => (
-                                <Field data-invalid={fieldState.invalid}>
-                                    <FieldLabel>Content</FieldLabel>
-                                    <QuillEditor
-                                        value={field.value}
-                                        onChange={field.onChange}
-                                    />
-                                    {fieldState.error && (
-                                        <FieldError errors={[fieldState.error]} />
-                                    )}
-                                </Field>
-                            )}
-                        />
-                    </FieldGroup>
-                </FieldGroup>
-            </FieldSet>
-            <div className="mt-15">
-                <Button type="submit">
-                    <SaveIcon></SaveIcon>
-                    {blogData ? 'Update' : 'Save'}
-                </Button>
-            </div>
-        </form>
-    )
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <FieldSet>
+        <FieldGroup>
+          <FieldGroup>
+            <Controller
+              name="title"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Title</FieldLabel>
+                  <Input {...field} type="text" />
+                  {fieldState.error && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="post_type"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldContent>
+                    <FieldLabel htmlFor="post-type">Post type</FieldLabel>
+                  </FieldContent>
+                  <Select
+                    name={field.name}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select post type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="Story">Story</SelectItem>
+                        <SelectItem value="Blog">Blog</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  {fieldState.error && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="content"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Content</FieldLabel>
+                  <QuillEditor value={field.value} onChange={field.onChange} />
+                  {fieldState.error && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </FieldGroup>
+      </FieldSet>
+      <div className="mt-15">
+        <Button type="submit">
+          <SaveIcon></SaveIcon>
+          {blogData ? "Update" : "Save"}
+        </Button>
+      </div>
+    </form>
+  );
 }
--- app/(admin)/admin/blogs/page.tsx
+++ app/(admin)/admin/blogs/page.tsx
@@ -1,63 +1,67 @@
-"use server"
+"use server";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import fetchPosts from "@/lib/fetch-posts";
+import deletePost from "@/lib/delete-post";
 
 export default async function Blogs() {
-
-    const posts = await fetchPosts()
-    return (
-        <section className="mx-auto max-w-7xl items-center justify-between p-6 lg:px-8">
-            <div className="bg-gray-300 p-2 rounded-xs">
-                Blogs
-            </div>
-            <div className="mt-2 mb-2">
-                <Button>
-                    <Link href='/admin/blogs/create'>+ Add blog</Link>
+  const posts = await fetchPosts();
+  return (
+    <section className="mx-auto max-w-7xl items-center justify-between p-6 lg:px-8">
+      <div className="bg-gray-300 p-2 rounded-xs">Blogs</div>
+      <div className="mt-2 mb-2">
+        <Button>
+          <Link href="/admin/blogs/create">+ Add blog</Link>
+        </Button>
+      </div>
+      <Table>
+        <TableCaption>A list of your Blogs.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-25">ID</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Created at</TableHead>
+            <TableHead>Post type</TableHead>
+            <TableHead>is Published</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {posts.map((post, index) => (
+            <TableRow key={post.id}>
+              <TableCell className="font-medium">{index + 1}</TableCell>
+              <TableCell>{post.title}</TableCell>
+              <TableCell>{new Date(post.createdAt).toLocaleString()}</TableCell>
+              <TableCell>{post.postType}</TableCell>
+              <TableCell>
+                {post.published ? "Published" : "Not published"}
+              </TableCell>
+              <TableCell className="text-right">
+                {/* <Button variant={"default"}>
+                  <Link href="">View</Link>
+                </Button> */}
+                <Button variant={"secondary"}>
+                  <Link href={`/admin/blogs/${post.id}/edit`}>Edit</Link>
                 </Button>
-            </div>
-            <Table>
-                <TableCaption>A list of your Blogs.</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-25">ID</TableHead>
-                        <TableHead>Title</TableHead>
-                        <TableHead>Created at</TableHead>
-                        <TableHead>is Published</TableHead>
-                        <TableHead className="text-right">Action</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {posts.map((post, index) => (
-                        <TableRow key={post.id}>
-                            <TableCell className="font-medium">{index + 1}</TableCell>
-                            <TableCell>{ post.title }</TableCell>
-                            <TableCell>{ new Date(post.createdAt).toLocaleString() }</TableCell>
-                            <TableCell>{ post.published ? 'Published' : 'Not published' }</TableCell>
-                            <TableCell className="text-right">
-                                <Button variant={"default"}>
-                                    <Link href="">View</Link>
-                                </Button>
-                                <Button variant={"secondary"}>
-                                    <Link href={`/admin/blogs/${post.id}/edit`}>Edit</Link>
-                                </Button>
-                                <Button variant={"destructive"}>
-                                    <Link href="">Delete</Link>
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </section>
-    )
+                <form action={deletePost.bind(null, post.id)}>
+                  <Button variant={"destructive"} type="submit">
+                    Delete
+                  </Button>
+                </form>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </section>
+  );
 }
