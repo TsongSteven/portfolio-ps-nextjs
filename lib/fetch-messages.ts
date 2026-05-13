c8f1e3f16c51be5df0c60663c8263f679270b2f1
Pranan Subba <pranan.steven@ymail.com>
2026-05-13 15:39:01 +0530

    Added date time in messages

--- lib/fetch-messages.ts
+++ lib/fetch-messages.ts
@@ -1,7 +1,9 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import { db } from "./prisma";
 
 export default async function fetchMessages() {
-    return await db.message.findMany()
+    return await db.message.findMany();
+    revalidatePath("/admin/messages");
 }
