42d0f130c49e8d46078d521b256735abcc835b65
Pranan Subba <pranan.steven@ymail.com>
2026-05-14 15:09:38 +0530

    Added unread messages

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import React from "react";
 import './../../globals.css';
+import getNoOfUnreadMsgs from "@/lib/unread-messages";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-mono' });
 
@@ -17,18 +18,20 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const unreadCount = await getNoOfUnreadMsgs();
+
     return (
         <html lang="en"
             suppressHydrationWarning
             className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-mono", jetbrainsMono.variable)}>
             <body suppressHydrationWarning>
                 <SidebarProvider>
-                    <AdminSidebar />
+                    <AdminSidebar count={unreadCount} />
                     <SidebarInset>
                         <main>{children}</main>
                     </SidebarInset>
